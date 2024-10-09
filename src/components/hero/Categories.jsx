aef821afcc767876dde0a3a7c47dcf3ba1c3f276
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-10-09 11:45:48 +0600

    category added

--- src/components/hero/Categories.jsx
+++ src/components/hero/Categories.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react';
+
+import React, { useEffect, useState } from 'react';
 
 const Categories = () => {
+    const [categoryData, setCategoryData] = useState([])
    
     useEffect(()=>{
-        fetch('categories.json')
-        .then(res => res.JSON())
-        .data(data=> console.log(data))
+        fetch('./././categories.json')
+        .then(res => res.json())
+        .then(data=> setCategoryData(data))
     },[])
     
     return (
         <div>
-            
+            <button className='btn btn-default w-full m-3'>National News</button>
+            {
+                categoryData.map(categroy => <ol className='font-bold text-left mx-7 my-2 text-gray-400'><li>{categroy.name}</li></ol>)
+            }
         </div>
     );
 };
