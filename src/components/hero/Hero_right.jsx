d93e1b48bfc2908459f429276188265054a79919
Jewel Hasan <jewelhasan.eee@gmail.com>
2024-10-13 07:38:57 +0600

    hero right side added

--- src/components/hero/Hero_right.jsx
+++ src/components/hero/Hero_right.jsx
@@ -1,12 +1,28 @@
-import { FaGoogle, FaGithub  } from "react-icons/fa";
+import { FaGoogle, FaGithub, FaInstagram, FaFacebook  } from "react-icons/fa";
 
 const Hero_right = () => {
     return (
         <div>
+            <div>
             <h1 className='font-bold mx-3 text-left font-bold'>Login With</h1>
             <button className=" btn btn-ghost border-black w-full mb-3 "><FaGoogle />Login with Google</button>
             <button className=" btn btn-ghost border-black w-full mb-3"> <FaGithub />Login with Github</button>
+            </div>
+            <div>
             <h1 className="text-left font-bold">Find us on</h1>
+            <button className=" btn btn-ghost border-black w-full mb-3 "><FaFacebook />Facebook</button>
+            <button className=" btn btn-ghost border-black w-full mb-3"> <FaGithub />  Github</button>
+            <button className=" btn btn-ghost border-black w-full mb-3"> <FaInstagram />  Github</button>
+            </div>
+           <div>
+           <h1 className="text-left font-bold">Q-zpne</h1>
+           <div className=" text-center">
+           <img src="./qZone1.png" alt="" />
+           <img src="./qZone2.png" alt="" />
+           <img src="./qZone3.png" alt="" />
+         
+           </div>
+           </div>
         </div>
     );
 };
