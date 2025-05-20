528c725fd6a0c4c183bb914c548fe7e1a09c39ae
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-05-20 16:01:16 +0600

    final

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,28 @@
-import Header from "../header/Header";
+
 import Marquee from "react-fast-marquee";
 import Navbar from "../Navbar/Navbar";
 import Hero from "../hero/Hero";
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
+const [marqueeData, setMarqueeData] = useState([])
+
+ useEffect(()=>{
+        fetch('./././news.json')
+        .then(res => res.json())
+        .then(data=> setMarqueeData(data))
+    },[])
+
     return (
       
       <div>
         <div className="flex">
       <button className="btn btn-error">Latest</button>
        <Marquee className="mx-3">
-       <p className="mx-3"> I can be a React component, multiple React components, or just some text.</p>
-       <p className="mx-3"> I can be a React component, multiple React components, or just some text.</p>
-       <p className="mx-3"> I can be a React component, multiple React components, or just some text.</p>
+       {marqueeData.map((marq)=>(
+        <p key={marq.id} className="mr-4 bg-slate-200 rounded-sm p-3 ">{marq.title} </p>
+       ))}
         </Marquee>
       </div>
       <div>
